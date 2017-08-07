79e7289f01e6d07c408315dad5bffd2b3054d6de
Jordan Hudgens <jordan@devcamp.com>
2017-08-07 16:57:10 +0000

    Refactored before action for portfolio setting

--- DevcampPortfolio/config/routes.rb
+++ DevcampPortfolio/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :blogs
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
@@ -14,4 +15,6 @@ Rails.application.routes.draw do
   end
 
   root to: 'pages#home'
+  
+  get 'guides/book'
 end
