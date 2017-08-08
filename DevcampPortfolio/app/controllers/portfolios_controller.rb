f00db24e430190863470d373284db039e035035b
Jordan Hudgens <jordan@devcamp.com>
2017-08-08 14:59:55 +0000

    Implemented bootstrap gem and styles

--- DevcampPortfolio/app/controllers/portfolios_controller.rb
+++ DevcampPortfolio/app/controllers/portfolios_controller.rb
@@ -1,26 +1,26 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   
   def index
     @portfolio_items = Portfolio.all
   end
-  
+
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(portfolios_params)
-    
+    @portfolio_item = Portfolio.new(portfolio_params)
+
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your Portfolio is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
@@ -29,11 +29,11 @@ class PortfoliosController < ApplicationController
 
   def edit
   end
-    
+
   def update
     respond_to do |format|
-      if @portfolio_item.update(portfolios_params)
-        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -42,29 +42,28 @@ class PortfoliosController < ApplicationController
 
   def show
   end
-  
+
   def destroy
     # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
+
   private
-  
-  def portfolios_params
+
+  def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
                                       :body,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
 
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
-
 end
